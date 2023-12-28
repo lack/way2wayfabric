1b033ed5c60438cf97e3b580b3e6d61199314997
Jim Ramsay <i.am@jimramsay.com>
2023-12-28 04:16:45 -0500

    Standardize source code via ktlint
    Signed-off-by: Jim Ramsay <i.am@jimramsay.com>

--- src/main/kotlin/com/jramsay/way2wayfabric/FabricWaystones.kt
+++ src/main/kotlin/com/jramsay/way2wayfabric/FabricWaystones.kt
@@ -3,7 +3,6 @@ package com.jimramsay.way2wayfabric
 import net.fabricmc.loader.api.FabricLoader
 import wraith.fwaystones.FabricWaystones
 import wraith.fwaystones.integration.event.WaystoneEvents
-import wraith.fwaystones.util.Utils
 
 object FabricWaystones : IWaystoneProvider {
     override val isPresent: Boolean
@@ -14,25 +13,28 @@ object FabricWaystones : IWaystoneProvider {
     override fun register(handler: IWay2WayHandler) {
         WaystoneEvents.DISCOVER_WAYSTONE_EVENT.register {
             val waystone = waystoneFromHash(it)
-            if (waystone != null)
+            if (waystone != null) {
                 handler.syncWaystone(waystone)
+            }
         }
         WaystoneEvents.RENAME_WAYSTONE_EVENT.register {
             val waystone = waystoneFromHash(it)
-            if (waystone != null)
+            if (waystone != null) {
                 handler.syncWaystone(waystone)
+            }
         }
         WaystoneEvents.REMOVE_WAYSTONE_EVENT.register {
             val waystone = waystoneFromHash(it)
-            if (waystone != null)
+            if (waystone != null) {
                 handler.removeWaystone(waystone)
+            }
         }
         WaystoneEvents.FORGET_ALL_WAYSTONES_EVENT.register {
             handler.removeAllWaystones(modIdx)
         }
     }
 
-    fun waystoneFromHash(hash: String?): GenericWaystone?     {
+    fun waystoneFromHash(hash: String?): GenericWaystone? {
         if (hash == null) return null
         val waystone = FabricWaystones.WAYSTONE_STORAGE.getWaystoneData(hash) ?: return null
         val pos = waystone.way_getPos().up(1)
--- src/main/kotlin/com/jramsay/way2wayfabric/Way2WayFabric.kt
+++ src/main/kotlin/com/jramsay/way2wayfabric/Way2WayFabric.kt
@@ -2,45 +2,51 @@ package com.jimramsay.way2wayfabric
 
 import net.fabricmc.api.ModInitializer
 import net.fabricmc.loader.api.FabricLoader
-import net.minecraft.client.MinecraftClient
 import net.minecraft.util.math.BlockPos
 import org.slf4j.LoggerFactory
-import java.util.Timer
-import java.util.LinkedList
-import kotlin.concurrent.schedule
-import kotlin.random.Random
-
-import xaero.common.minimap.waypoints.WaypointSet
+import xaero.common.XaeroMinimapSession
 import xaero.common.minimap.waypoints.Waypoint
+import xaero.common.minimap.waypoints.WaypointSet
 import xaero.common.minimap.waypoints.WaypointsManager
 import xaero.common.settings.ModSettings
-import xaero.common.XaeroMinimapSession
 import xaero.minimap.XaeroMinimap
+import java.util.LinkedList
+import java.util.Timer
+import kotlin.concurrent.schedule
+import kotlin.random.Random
 
 val logger = LoggerFactory.getLogger("Way2WayFabric")
 
 interface IWay2WayHandler {
     fun syncWaystone(waystone: GenericWaystone)
-    fun syncAllWaystones(waystones: List<GenericWaystone>, modIdx: Int)
+
+    fun syncAllWaystones(
+        waystones: List<GenericWaystone>,
+        modIdx: Int,
+    )
+
     fun removeWaystone(waystone: GenericWaystone)
+
     fun removeAllWaystones(modIdx: Int)
 }
 
 interface IWaystoneProvider {
     val isPresent: Boolean
     var modIdx: Int
+
     fun register(handler: IWay2WayHandler)
 }
 
 data class GenericWaystone(val x: Int, val y: Int, val z: Int, val name: String, val dimension: String, val modIdx: Int) {
-    constructor(pos : BlockPos, name: String, dimension: String, modIdx: Int) : this(pos.x, pos.y, pos.z, name, dimension, modIdx)
+    constructor(pos: BlockPos, name: String, dimension: String, modIdx: Int) : this(pos.x, pos.y, pos.z, name, dimension, modIdx)
 
     companion object {
         val SYMBOL = arrayOf("〨", "兰", "亗", "主")
 
         fun symbol(idx: Int): String {
-            if (idx < 0)
+            if (idx < 0) {
                 return SYMBOL[0]
+            }
             return SYMBOL[idx]
         }
     }
@@ -77,7 +83,7 @@ fun Waypoint.fromMod(modIdx: Int): Boolean {
 fun WaypointSet.updateWaypointFor(waystone: GenericWaystone): Boolean {
     val new = waystone.toWaypoint()
     logger.debug("Updating waypoint for $waystone")
-    val existing = list.find{it.matches(new)}
+    val existing = list.find { it.matches(new) }
     if (existing == null) {
         logger.info("Adding new waypoint for $waystone")
         list.add(new)
@@ -93,12 +99,12 @@ fun WaypointSet.updateWaypointFor(waystone: GenericWaystone): Boolean {
 }
 
 class MapWatcher {
-    private val deferred = LinkedList<(WaypointsManager)->Unit>()
+    private val deferred = LinkedList<(WaypointsManager) -> Unit>()
     private var running = false
     private var count = 0
 
     @Synchronized
-    fun whenReady(fn : (WaypointsManager)->Unit) {
+    fun whenReady(fn: (WaypointsManager) -> Unit) {
         val mgr = mgrIfReady()
         if (mgr == null || deferred.isNotEmpty()) {
             defer(fn)
@@ -110,13 +116,14 @@ class MapWatcher {
 
     fun mgrIfReady(): WaypointsManager? {
         val mgr = XaeroMinimapSession.getCurrentSession()?.waypointsManager
-        if (mgr?.currentWorld == null)
+        if (mgr?.currentWorld == null) {
             return null
+        }
         return mgr
     }
 
     @Synchronized
-    private fun defer(fn : (WaypointsManager)->Unit) {
+    private fun defer(fn: (WaypointsManager) -> Unit) {
         if (givenUp()) return
         logger.debug("Deferring $fn")
         deferred.add(fn)
@@ -133,14 +140,17 @@ class MapWatcher {
         val mgr = mgrIfReady()
         if (mgr == null) {
             if (givenUp()) {
-                logger.warn("Waypoint manager is not ready after $count attempts. Giving up and discarding ${deferred.size} deferred events")
+                logger.warn(
+                    "Waypoint manager is not ready after $count attempts. Giving up and discarding ${deferred.size} deferred events",
+                )
                 running = false
                 return
             }
-            if (!running)
+            if (!running) {
                 logger.info("Defering waystone events until the waypoint manager is ready")
-            else
+            } else {
                 logger.debug("Setting deferred event timer ($count)")
+            }
             running = true
             count++
             Timer("Deferred Events", false).schedule(500) {
@@ -149,7 +159,7 @@ class MapWatcher {
             return
         }
         logger.info("Waypoint manager is ready. Running ${deferred.size} deferred events")
-        deferred.forEach{
+        deferred.forEach {
             it(mgr)
         }
         deferred.clear()
@@ -159,19 +169,21 @@ class MapWatcher {
 }
 
 @Suppress("UNUSED")
-object Way2WayFabric: ModInitializer, IWay2WayHandler {
+object Way2WayFabric : ModInitializer, IWay2WayHandler {
     const val MOD_ID = "way2wayfabric"
     private val mapWatcher = MapWatcher()
     private val compatibleMaps = arrayOf("xaerominimap", "xaerominimapfair")
 
     fun ensureCompatibleMap() {
-        val matched = FabricLoader.getInstance().allMods.filter{
-            mod -> compatibleMaps.any { it == mod.metadata.id }
-        }
+        val matched =
+            FabricLoader.getInstance().allMods.filter {
+                    mod ->
+                compatibleMaps.any { it == mod.metadata.id }
+            }
         if (matched.any()) {
-            logger.info("Found compatible map mod ${matched}")
+            logger.info("Found compatible map mod $matched")
         } else {
-            logger.warn("No compatible map mods are loaded! (Expecting one of: ${compatibleMaps})")
+            logger.warn("No compatible map mods are loaded! (Expecting one of: $compatibleMaps)")
         }
     }
 
@@ -188,79 +200,98 @@ object Way2WayFabric: ModInitializer, IWay2WayHandler {
             logger.info("Registered with FabricWaystones for waypoint sync")
             providers.add(FabricWaystones)
         }
-        if (providers.size == 1)
+        if (providers.size == 1) {
             providers.first.modIdx = -1
-        else
+        } else {
             providers.forEachIndexed {
-                i, it -> it.modIdx = i
+                    i, it ->
+                it.modIdx = i
             }
+        }
         logger.info("Way2wayFabric has been initialized for ${providers.size} waystone provider(s)")
     }
 
-    override fun syncAllWaystones(waystones: List<GenericWaystone>, modIdx: Int) { mapWatcher.whenReady { mgr ->
-        logger.debug("Known: ${waystones.size} waystones")
-        val waypointSet = mgr.currentWorld.currentSet
+    override fun syncAllWaystones(
+        waystones: List<GenericWaystone>,
+        modIdx: Int,
+    ) {
+        mapWatcher.whenReady { mgr ->
+            logger.debug("Known: ${waystones.size} waystones")
+            val waypointSet = mgr.currentWorld.currentSet
 
-        var stale = waypointSet.list.filter {
-            it.fromMod(modIdx)
-        }
+            var stale =
+                waypointSet.list.filter {
+                    it.fromMod(modIdx)
+                }
 
-        var changed = 0
-        waystones.filter {
-            mgr.sameDimensionAs(it)
-        }.forEach {
-            if (waypointSet.updateWaypointFor(it))
-                changed += 1
-            stale = stale.filterNot {
-                wp -> wp.matches(it)
+            var changed = 0
+            waystones.filter {
+                mgr.sameDimensionAs(it)
+            }.forEach {
+                if (waypointSet.updateWaypointFor(it)) {
+                    changed += 1
+                }
+                stale =
+                    stale.filterNot {
+                            wp ->
+                        wp.matches(it)
+                    }
             }
-        }
 
-        if (stale.size > 0) {
-            waypointSet.list.removeAll(stale)
-        }
-        
-        if (changed > 0 || stale.size > 0) {
-            logger.info("Synchronized $changed waystone waypoints and removed ${stale.size} stale entries")
-            XaeroMinimap.instance.settings.saveAllWaypoints(mgr)
-        }
-    } }
+            if (stale.size > 0) {
+                waypointSet.list.removeAll(stale)
+            }
 
-    override fun syncWaystone(waystone: GenericWaystone) { mapWatcher.whenReady { mgr ->
-        logger.debug("Update: ${waystone}")
-        val waypointSet = mgr.currentWorld.currentSet
-
-        if (mgr.sameDimensionAs(waystone)) {
-            if (waypointSet.updateWaypointFor(waystone)) {
-                logger.debug("Updated $waystone")
+            if (changed > 0 || stale.size > 0) {
+                logger.info("Synchronized $changed waystone waypoints and removed ${stale.size} stale entries")
                 XaeroMinimap.instance.settings.saveAllWaypoints(mgr)
             }
         }
-    } }
+    }
 
-    override fun removeWaystone(waystone: GenericWaystone) { mapWatcher.whenReady { mgr ->
-        logger.debug("Removing waypoint for $waystone")
-        val waypointSet = mgr.currentWorld.currentSet
+    override fun syncWaystone(waystone: GenericWaystone) {
+        mapWatcher.whenReady { mgr ->
+            logger.debug("Update: $waystone")
+            val waypointSet = mgr.currentWorld.currentSet
 
-        val changed = waypointSet.list.removeIf {
-            it.matches(waystone)
+            if (mgr.sameDimensionAs(waystone)) {
+                if (waypointSet.updateWaypointFor(waystone)) {
+                    logger.debug("Updated $waystone")
+                    XaeroMinimap.instance.settings.saveAllWaypoints(mgr)
+                }
+            }
         }
-        if (changed) {
-            logger.info("Removed waypoint for $waystone")
-            XaeroMinimap.instance.settings.saveAllWaypoints(mgr)
-        }
-    } }
+    }
 
-    override fun removeAllWaystones(modIdx: Int) { mapWatcher.whenReady { mgr ->
-        logger.debug("Removing all waystone waypoints")
-        val waypointSet = mgr.currentWorld.currentSet
+    override fun removeWaystone(waystone: GenericWaystone) {
+        mapWatcher.whenReady { mgr ->
+            logger.debug("Removing waypoint for $waystone")
+            val waypointSet = mgr.currentWorld.currentSet
 
-        val changed = waypointSet.list.removeIf {
-            it.fromMod(modIdx)
+            val changed =
+                waypointSet.list.removeIf {
+                    it.matches(waystone)
+                }
+            if (changed) {
+                logger.info("Removed waypoint for $waystone")
+                XaeroMinimap.instance.settings.saveAllWaypoints(mgr)
+            }
         }
-        if (changed) {
-            logger.info("Removed all waystone waypoints")
-            XaeroMinimap.instance.settings.saveAllWaypoints(mgr)
+    }
+
+    override fun removeAllWaystones(modIdx: Int) {
+        mapWatcher.whenReady { mgr ->
+            logger.debug("Removing all waystone waypoints")
+            val waypointSet = mgr.currentWorld.currentSet
+
+            val changed =
+                waypointSet.list.removeIf {
+                    it.fromMod(modIdx)
+                }
+            if (changed) {
+                logger.info("Removed all waystone waypoints")
+                XaeroMinimap.instance.settings.saveAllWaypoints(mgr)
+            }
         }
-    } }
+    }
 }
